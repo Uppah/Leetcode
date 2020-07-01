0ee8d50437429b5d2942402d2dcc2d627f61138d
Philip Thorsen <37532290+Uppah@users.noreply.github.com>
2020-07-01 14:08:22 +0200

    Create Jewels_and_Stones.java

---
+++ Java/Jewels_and_Stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int ans = 0;
+
+        for(int i = 0; i < S.length(); i++){
+            for(int j = 0; j < J.length(); j++){
+                if(J.charAt(j) == S.charAt(i)){
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
